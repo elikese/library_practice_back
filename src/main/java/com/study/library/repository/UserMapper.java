c4965855c801604670707adf414b7ce84f8ede94
elikese <elikese@gmail.com>
2024-03-22 17:45:51 +0900

    비밀번호 변경ì구현 완료

--- src/main/java/com/study/library/repository/UserMapper.java
+++ src/main/java/com/study/library/repository/UserMapper.java
@@ -10,12 +10,10 @@ import org.apache.ibatis.annotations.Param;
 public interface UserMapper {
     User findUserByUserName(String username);
     int saveUser(User user);
-
     int saveOAuth2(OAuth2 oAuth2);
     RoleRegister findRoleRegisterByUserIdAndRoleId(@Param("userId") int userId, @Param("roleId") int roleId);
-
     int saveRole(@Param("userId") int userId, @Param("roleId") int roleId);
-
     User findUserByOAuth2name(String oAuth2name);
+    int modifyPassword(User user);
 
 }
--- src/main/java/com/study/library/service/AccountService.java
+++ src/main/java/com/study/library/service/AccountService.java
@@ -4,7 +4,6 @@ import com.study.library.dto.EditPasswordReqDto;
 import com.study.library.entity.User;
 import com.study.library.exception.ValidException;
 import com.study.library.repository.UserMapper;
-import com.study.library.security.PrincipalUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -23,15 +22,20 @@ public class AccountService {
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
 
+    @Transactional(rollbackFor = Exception.class)
     public void editPassword(EditPasswordReqDto reqDto) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         User user = userMapper.findUserByUserName(authentication.getName());
         if(!passwordEncoder.matches(reqDto.getOldPassword(), user.getPassword())) {
             throw new ValidException(Map.of("oldPassword","비밀번호 인증에 실패하였습니다 \n다시입력하세요"));
         }
-        if(!reqDto.getOldPassword().equals(reqDto.getNewPassword())) {
+        if(passwordEncoder.matches(reqDto.getNewPassword(), user.getPassword())) {
+            throw new ValidException(Map.of("newPassword","새로운 비밀번호는 이전 비밀번호와 같을 수 없습니다 \n다시입력하세요"));
+        }
+        if(!reqDto.getNewPasswordCheck().equals(reqDto.getNewPassword())) {
             throw new ValidException(Map.of("newPasswordCheck","새로운 비밀번호가 서로 일치하지 않습니다 \n다시입력하세요"));
         }
+        user.setPassword(passwordEncoder.encode(reqDto.getNewPassword()));
+        userMapper.modifyPassword(user);
     }
 }
-
